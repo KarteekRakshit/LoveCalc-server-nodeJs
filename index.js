74f5303bcbc9d24d330935a1517cc02c1267e3f1
Karteek Rakshit <karteek263@gmail.com.com>
2020-02-19 21:04:19 +0530

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var cors = require('cors')
 var bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
+
 const port = process.env.PORT || 3000;;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
